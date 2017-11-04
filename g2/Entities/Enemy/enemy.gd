58ad3ea11e14552093acde84fb46fdcda3d1a8f4
Lior David <liorda@gmail.com>
2017-11-04 16:00:39 +0200

    enemy animation working with particles effect (flowing and death)

--- g2/Entities/Enemy/enemy.gd
+++ g2/Entities/Enemy/enemy.gd
@@ -1,4 +1,4 @@
-extends RigidBody2D
+extends Area2D
 
 const max_time_to_course = 35.0
 const max_course_speed = 1.0
@@ -6,8 +6,12 @@ const course_added_noise = 0.0
 
 var time_to_course
 var course
+var dead_timestamp = -1
 
 onready var death_particle_effect = get_node("EnemyDeathParticles2D")
+onready var flowing_particle_effect = get_node("EnemyParticles2D")
+onready var sprite = get_node("EnemySprite")
+onready var col = get_node("EnemyCollisionShape2D")
 
 func init_course():
 	time_to_course = randf() * max_time_to_course # 0..10 secs
@@ -23,6 +27,13 @@ func _ready():
 	set_process(true)
 
 func _process(delta):
+	if dead_timestamp > 0:
+		var secs_since_death = (OS.get_ticks_msec() - dead_timestamp) / 1000.0
+		var death_anim_ended = secs_since_death > death_particle_effect.get_lifetime()
+		var flowing_anim_ended = secs_since_death > flowing_particle_effect.get_lifetime()
+		if death_anim_ended and flowing_anim_ended:
+			queue_free()
+		return
 	time_to_course -= delta
 	if time_to_course < 0.0:
 		init_course()
@@ -41,8 +52,13 @@ func _process(delta):
 		new_pos.y = get_viewport_rect().size.y
 		course.y = -course.y
 	var dir = new_pos - get_pos()
-	get_node("EnemyParticles2D").set_param(Particles2D.PARAM_DIRECTION, rad2deg(dir.angle()))
+	flowing_particle_effect.set_param(Particles2D.PARAM_DIRECTION, rad2deg(dir.angle()))
 	set_pos(new_pos)
 
-func _on_Area2D_area_enter( area ):
-	death_particle_effect.start()
+func _on_EnemyArea2D_area_enter( area ):
+	dead_timestamp = OS.get_ticks_msec()
+	death_particle_effect.set_emitting(true)
+	flowing_particle_effect.set_emitting(false)
+	sprite.hide()
+	set_layer_mask(0)
+	set_collision_mask(0)
