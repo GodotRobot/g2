7d3675cff42c95c2f0408598bac8d36a2d93115c
Lior David <liorda@gmail.com>
2017-11-05 23:28:59 +0200

    restart button on pause menu

--- g2/Menu/Menu.gd
+++ g2/Menu/Menu.gd
@@ -2,6 +2,7 @@ extends CanvasLayer
 
 onready var game = get_tree().get_root().get_node("game")
 onready var start_button = get_node("VBoxContainer/StartButton")
+onready var restart_button = get_node("VBoxContainer/RestartButton")
 onready var context = get_node("VBoxContainer/Context")
 
 enum MODE {
@@ -17,15 +18,19 @@ func _ready():
 	if mode == MODE.pause:
 		context.set_text("Paused")
 		start_button.set_text("Continue")
+		restart_button.show()
 	elif mode == MODE.game_over:
 		context.set_text("GAME OVER")
 		start_button.set_text("Restart")
+		restart_button.hide()
 	elif mode == MODE.next_level:
 		context.set_text("Level Completed")
 		start_button.set_text("Next Level")
+		restart_button.hide()
 	elif mode == MODE.win:
 		context.set_text("WIN")
 		start_button.set_text("Restart")
+		restart_button.hide()
 	set_process_input(true)
 
 func _input(event):
@@ -40,3 +45,7 @@ func _on_Start_Button_pressed():
 
 func _on_QuitButton_pressed():
 	get_tree().quit()
+
+func _on_RestartButton_pressed():
+	get_tree().change_scene("res://Game.tscn")
+	game.unpause(self)
