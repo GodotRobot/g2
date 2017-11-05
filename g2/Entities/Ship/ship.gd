d487a1af5fc9c83841fbba79afdae35277e5d997
Lior David <liorda@gmail.com>
2017-11-05 23:20:53 +0200

    ship death: logic + effect

--- g2/Entities/Ship/ship.gd
+++ g2/Entities/Ship/ship.gd
@@ -1,6 +1,12 @@
 extends Area2D
 
+var dead_timestamp = -1
+
 onready var game = get_tree().get_root().get_node("game")
+onready var death_particle_effect = get_node("ShipDeathParticles2D")
+onready var flowing_particle_effect = get_node("ShipParticles2D")
+onready var sprite = get_node("ShipSprite")
+onready var col = get_node("ShipCollisionShape2D")
 
 func ship_destroyed(area):
 	game.ship_destroyed()
@@ -11,6 +17,13 @@ func _ready():
 	set_process(true)
 
 func _process(delta):
+	if dead_timestamp > 0:
+		var secs_since_death = (OS.get_ticks_msec() - dead_timestamp) / 1000.0
+		var death_anim_ended = secs_since_death > death_particle_effect.get_lifetime()
+		var flowing_anim_ended = secs_since_death > flowing_particle_effect.get_lifetime()
+		if death_anim_ended and flowing_anim_ended:
+			queue_free()
+		return
 	if game.menu_displayed:
 		return
 	var v = Vector2(0.0, -1.0).rotated(get_rot())
@@ -37,4 +50,10 @@ func _process(delta):
 	rotate(delta_rad)
 
 func _on_ShipArea2D_area_enter( area ):
+	dead_timestamp = OS.get_ticks_msec()
+	death_particle_effect.set_emitting(true)
+	flowing_particle_effect.set_emitting(false)
+	sprite.hide()
+	set_layer_mask(0)
+	set_collision_mask(0)
 	ship_destroyed(area)
