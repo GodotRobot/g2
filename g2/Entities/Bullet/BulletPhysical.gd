d6d858f5daef60a34961ff5678dc2b9892fc1e8b
Lior David <liorda@gmail.com>
2017-11-09 23:42:15 +0200

    Fixing physical bullet

--- g2/Entities/Bullet/BulletPhysical.gd
+++ g2/Entities/Bullet/BulletPhysical.gd
@@ -3,10 +3,10 @@ extends RigidBody2D
 var v_
 
 func _ready():
-	set_linear_velocity(v_ * 120.0)
+	set_linear_velocity(v_ * 60.0)
 	set_process(true)
 
 func _process(delta):
-	var v = get_linear_velocity()
+	var v = get_pos()
 	if v.y < 0 or v.y > get_viewport_rect().size.y or v.x < 0 or v.x > get_viewport_rect().size.x:
 		queue_free()
