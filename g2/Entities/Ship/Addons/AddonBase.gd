0d16ad1ca120ccd8bbcc0613c4e68d03798fc965
Lior David <liorda@gmail.com>
2017-11-20 20:41:52 +0200

    addons functional

--- g2/Entities/Ship/Addons/AddonBase.gd
+++ g2/Entities/Ship/Addons/AddonBase.gd
@@ -1,6 +1,13 @@
 extends Sprite
 
-export(int, 0, 100) var power = 100
+# power is time in seconds until effect wears off
+export(int, 0, 100) var power = 0
+
+onready var timer = get_node("Timer")
 
 func _ready():
-	pass
+	timer.set_wait_time(float(power))
+	timer.start()
+
+func _on_Timer_timeout():
+	queue_free()
