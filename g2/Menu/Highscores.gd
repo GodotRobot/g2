f639922ff9dc3c875b49015868d0c86da858a02a
Lior David <liorda@gmail.com>
2017-11-22 00:50:52 +0200

    highscores retrieve done

--- g2/Menu/Highscores.gd
+++ g2/Menu/Highscores.gd
@@ -8,10 +8,8 @@ func _on_loaded(result):
 	#print(result_string)
 	var scores = {}
 	scores.parse_json(result_string)
-	var i=0
-	for pos in scores.pos:
-		get_node("VBoxContainer/GridContainer/Pos" + String(i+1)).set_text(pos)
-		i += 1
+	for i in range(5):
+		get_node("VBoxContainer/GridContainer/Pos" + String(i+1)).set_text(String(i+1))
 	var i=0
 	for name in scores.names:
 		get_node("VBoxContainer/GridContainer/Name" + String(i+1)).set_text(name)
@@ -25,7 +23,7 @@ func _ready():
 	var http = get_node("HTTP")
 	http.connect("loading",self,"_on_loading")
 	http.connect("loaded",self,"_on_loaded")
-	http.get("192.168.0.137","/",8000,false) #domain,url,port,useSSL
+	http.get("http://warpgamehighscores.azurewebsites.net","/",80,false) #domain,url,port,useSSL
 
 func _init():
 	pass
