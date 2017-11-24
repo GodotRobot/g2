5b80c7b60aac5188ac4d18944d27739416a06507
Lior David <liorda@gmail.com>
2017-11-24 23:31:45 +0200

    funcational shield addon

--- g2/Entities/Ship/Addons/AddonBase.gd
+++ g2/Entities/Ship/Addons/AddonBase.gd
@@ -1,14 +1,29 @@
 extends Sprite
 
+enum TYPE {
+	shield = 0,
+	cannon = 1
+}
+
+const KIND_TO_TYPE = {
+	"Shield" : TYPE.shield,
+	"Cannon" : TYPE.cannon
+}
 # power is time in seconds until effect wears off
 export(int, 0, 100) var power = 0
+export(String, "Shield", "Cannon") var kind = "Shield"
 
 onready var timer = get_node("Timer")
+var type = TYPE.shield
 
 func _ready():
+	type = KIND_TO_TYPE[kind]
 	if power > 0:
 		timer.set_wait_time(float(power))
 		timer.start()
 
 func _on_Timer_timeout():
 	queue_free()
+
+func _on_ShieldArea_body_enter( body ):
+	body.start_death()
